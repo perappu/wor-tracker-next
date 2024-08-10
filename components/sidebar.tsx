c24fd4fe27289d2b87204395833b62cd685a0a27
Kazimir <stokoriel@gmail.com>
2024-08-10 09:13:38 -0500

    please just accept this

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-export default function Sidebar({ dropdowns }) {
+export default function Sidebar(dropdowns : any) {
 
   return (<>
     <nav className="h-screen bg-slate-800 p-5 pt-8 w-72 transition-all duration-300 relative prose prose-slate !prose-invert">
