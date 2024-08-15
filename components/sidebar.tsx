08013853762b2a37b74c0452dc142c947c22c12c
Kazimir <stokoriel@gmail.com>
2024-08-14 19:48:47 -0500

    add zenith

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -9,9 +9,9 @@ export default function Sidebar({ children }: ScriptProps) {
   const [sidebarOpen, setSidebarOpen] = useState(true);
 
   return (<>
-  <div onClick={() => setSidebarOpen(!sidebarOpen)} className={`absolute top-10 left-10 bg-slate-800 text-white text-xl z-10 outline rounded-md ${sidebarOpen ? "lg:left-60" : "lg:left-16"}`}><IoMenu /></div>
-    <nav className={`h-screen sticky top-0 bg-slate-800 p-5 pt-8 transition-all duration-300 relative prose prose-slate !prose-invert ${sidebarOpen ? "max-w-full opacity-100 lg:block lg:w-72" : "p-0 opacity-0 lg:opacity-100 lg:block lg:w-20"}`}>
-    
+    <nav className={`h-screen sticky top-0 bg-slate-800 p-5 pt-8 transition-all duration-300 relative prose prose-slate !prose-invert ${sidebarOpen ? "max-w-full opacity-100 lg:block lg:w-72" : "lg:opacity-100 lg:block lg:w-20"}`}>
+    <div onClick={() => setSidebarOpen(!sidebarOpen)} className={`relative w-7 bg-slate-800 text-white text-xl z-10 p-1 outline rounded-md text-center`}><IoMenu /></div>
+
       <div className={`${sidebarOpen ? "visible" : "hidden"}`}>
         <a href="/"><h2>Stokori&apos;s Reos Tracker</h2></a>
       {children}</div>
